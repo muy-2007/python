66ded8bc6820f877a4916624a9bfd772ab465b57
muy <maryanne.uy2007@gmail.com>
2026-04-10 13:08:22 -0500

    Made all methods, most empty

--- television.py
+++ television.py
@@ -5,5 +5,30 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
+        status = False
+        muted = False
+        volume = Television.MIN_VOLUME
+        channel = Television.MIN_CHANNEL
+
+    def power(self):
         pass
+
+    def mute(self):
+        pass
+
+    def channel_up(self):
+        pass
+
+    def channel_down(self):
+        pass
+
+    def volume_up(self):
+        pass
+
+    def volume_down(self):
+        pass
+
+    def __str__(self):
+        pass
+
 
