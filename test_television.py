80d3a373cec2da6eea60437a8052f5452773b7a1
muy <maryanne.uy2007@gmail.com>
2026-04-14 21:14:09 -0500

    creating test file

---
+++ test_television.py
@@ -0,0 +1,25 @@
+from pytest import *
+from television import *
+
+
+def test_init():
+    pass
+
+def test_power():
+    pass
+
+def test_mute():
+    pass
+
+def channel_up():
+    pass
+
+def channel_down():
+    pass
+
+def volume_up():
+    pass
+
+def volume_down():
+    pass
+
