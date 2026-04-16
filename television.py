842f8d3c4ac2b5f48d0bde0d455c24c9139ecb44
muy <maryanne.uy2007@gmail.com>
2026-04-16 15:46:19 -0500

    added type hinting to methods, fixed quotations in docstrings, made instance variables private

--- television.py
+++ television.py
@@ -5,114 +5,104 @@ class Television:
     MIN_CHANNEL: int = 0
     MAX_CHANNEL: int = 3
 
-    def __init__(self):
-        '''
+    def __init__(self) -> None:
+        """
         Used to set the default instance variables
         :return: None
-        '''
+        """
 
-        self.status: bool = False
-        self.muted: bool = False
-        self.volume: int = Television.MIN_VOLUME
-        self.channel: int = Television.MIN_CHANNEL
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = Television.MIN_VOLUME
+        self.__channel: int = Television.MIN_CHANNEL
 
-    def power(self):
-        '''
+    def power(self) -> None:
+        """
         Used to determine the current status of the television
         :return: None
-        '''
+        """
 
-        self.status: bool
-        if self.status:
-            self.status = False
-            self.muted = True
-            self.channel = 1
-        elif not self.status:
-            self.status = True
-            self.muted = False
-            self.volume = Television.MIN_VOLUME
-            self.channel = Television.MIN_CHANNEL
+        if self.__status:
+            self.__status = False
+            self.__muted = True
+            self.__channel = 1
+        elif not self.__status:
+            self.__status = True
+            self.__muted = False
+            self.__volume = Television.MIN_VOLUME
+            self.__channel = Television.MIN_CHANNEL
 
-    def mute(self):
-        '''
+    def mute(self) -> None:
+        """
         Used to make the television muted
         :return: None
-        '''
+        """
 
-        self.muted: bool
-        if self.status:
-            if self.muted:
-                self.muted = False
-                self.volume = Television.MIN_VOLUME + 1
-            elif not self.muted:
-                self.muted = True
-                self.volume = Television.MIN_VOLUME
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+                self.__volume = Television.MIN_VOLUME + 1
+            elif not self.__muted:
+                self.__muted = True
+                self.__volume = Television.MIN_VOLUME
 
-    def channel_up(self):
-        '''
+    def channel_up(self) -> None:
+        """
         Used to raise the channel
         :return: None
-        '''
+        """
 
-        self.channel: int
-
-        if not self.status:
-            self.channel = 0
-        elif self.channel == Television.MAX_CHANNEL and self.status:
-            self.channel = Television.MIN_CHANNEL
+        if not self.__status:
+            self.__channel = 0
+        elif self.__channel == Television.MAX_CHANNEL and self.__status:
+            self.__channel = Television.MIN_CHANNEL
         else:
-            self.channel += 1
+            self.__channel += 1
 
-    def channel_down(self):
-        '''
+    def channel_down(self) -> None:
+        """
         Used to lower the channel
         :return: None
-        '''
+        """
 
-        self.channel: int
-
-        if not self.status:
-            self.channel = 0
-        elif self.channel == Television.MIN_CHANNEL and self.status:
-            self.channel = Television.MAX_CHANNEL
+        if not self.__status:
+            self.__channel = 0
+        elif self.__channel == Television.MIN_CHANNEL and self.__status:
+            self.__channel = Television.MAX_CHANNEL
         else:
-            self.channel -= 1
+            self.__channel -= 1
 
-    def volume_up(self):
-        '''
+    def volume_up(self) -> None:
+        """
         Used to raise the volume
         :return: None
-        '''
+        """
 
-        self.volume: int
-
-        if not self.status or self.muted:
-            self.volume = self.MIN_VOLUME
-        elif self.volume == Television.MAX_VOLUME:
-            self.volume = Television.MIN_VOLUME
+        if not self.__status or self.__muted:
+            self.__volume = self.MIN_VOLUME
+        elif self.__volume == Television.MAX_VOLUME:
+            self.__volume = Television.MIN_VOLUME
         else:
-            self.volume += 1
+            self.__volume += 1
 
-    def volume_down(self):
-        '''
+    def volume_down(self) -> None:
+        """
         Used to lower the volume
         :return: None
-        '''
+        """
 
-        self.volume: int
-
-        if not self.status or self.muted:
-            self.volume = self.MIN_VOLUME
-        elif self.volume == Television.MIN_VOLUME and self.status:
-            self.volume = Television.MAX_VOLUME
+        if not self.__status or self.__muted:
+            self.__volume = self.MIN_VOLUME
+        elif self.__volume == Television.MIN_VOLUME and self.__status:
+            self.__volume = Television.MAX_VOLUME
         else:
-            self.volume -= 1
+            self.__volume -= 1
 
-    def __str__(self):
-        '''
+    def __str__(self) -> str:
+        """
         Used to format the values of the television object
         :return: Three f strings, values of status, channel, and volume
-        '''
-        return (f'Power = {self.status}\nChannel = {self.channel}\nVolume = {self.volume}\n')
+        """
+        return f'Power = {self.__status}\nChannel = {self.__channel}\nVolume = {self.__volume}\n'
 
 
